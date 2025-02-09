142755978f112d823ee3e60f906be98d7d9a334c
Biswa Prakash Mishra <biswaprakash.mishra@infinitylearn.com>
2025-02-09 15:02:11 +0530

    errors comparison changed

--- crypto.go
+++ crypto.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 func hashPassword(password string) (string, error) {
 	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	fmt.Println("password: ", string(hashedBytes))
 	return string(hashedBytes), err
 }
 
 func checkPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	return err == nil
+	return errors.Is(err, nil)
 }
